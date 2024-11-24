6f4f1fae0290c534fffe525702d31c90acfcc667
IdMarchena <idmarchena@gmail.com>
2024-11-24 12:39:58 -0500

    [ADD] reeconstruction client/mapper

--- src/main/java/com/example/airport/mapper/ClientMapper.java
+++ src/main/java/com/example/airport/mapper/ClientMapper.java
@@ -1,4 +1,37 @@
 package com.example.airport.mapper;
 
+import com.example.airport.dtos.ClientDto;
+import com.example.airport.entities.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = ReserveMapper.class)
 public interface ClientMapper {
+    @Named("complete")
+    @Mapping(source = "client.reserves", target = "reserves", qualifiedByName = "listComplete")
+    ClientDto toIdDto(Client client);
+
+    @Named("listComplete")
+    @Mapping(source = "client.reserves", target = "reserves", qualifiedByName = "listComplete")
+    List<ClientDto> toListIdDto(List<Client> clients);
+
+    @Mapping(source = "clientDto.reserves", target = "reserves")
+    Client toEntity(ClientDto clientDto);
+
+
+    @Mapping(source = "clientDto.reserves", target = "reserves")
+    List<Client> toListEntity(List<ClientDto> clientDto);
+
+    @Named("withoutId")
+    @Mapping(target = "id",ignore = true)
+    @Mapping(source = "client.reserves", target = "reserves", qualifiedByName = "listWithoutId")
+    ClientDto toDto(Client client);
+
+    @Named("listWithoutId")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "client.reserves", target = "reserves", qualifiedByName = "listWithoutId")
+    List<ClientDto> toListDto(List<Client> clients);
 }
