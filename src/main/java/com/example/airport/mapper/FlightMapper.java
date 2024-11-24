43dd1a0b9dd55d4238ef90768a8ae0abbe101d3e
IdMarchena <idmarchena@gmail.com>
2024-11-24 13:50:55 -0500

    [UPDATE] flight/mapper

--- src/main/java/com/example/airport/mapper/FlightMapper.java
+++ src/main/java/com/example/airport/mapper/FlightMapper.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public abstract class FlightMapper {
+
     @Autowired
     private AirlaneRepository airlaneRepository;
     @Autowired
@@ -26,75 +27,76 @@ public abstract class FlightMapper {
     private ReserveRepository reserveRepository;
 
     @Named("complete")
-    @Mapping(source = "flight.airlane.id",target = "airlane")
+    @Mapping(source = "flight.airlane.id", target = "airlane")
     @Mapping(source = "flight.airport.id", target = "airport")
     @Mapping(source = "flight.reserves", target = "reserves")
     public abstract FlightDto toIdDto(Flight flight);
 
     @Named("listComplete")
-    @Mapping(source = "flight.airlane.id",target = "airlane")
+    @Mapping(source = "flight.airlane.id", target = "airlane")
     @Mapping(source = "flight.airport.id", target = "airport")
     @Mapping(source = "flight.reserves", target = "reserves")
     public abstract List<FlightDto> toListIdDto(List<Flight> flights);
 
-    @Mapping(source = "flightDto.airlane",target = "airlane")
+    @Mapping(source = "flightDto.airlane", target = "airlane")
     @Mapping(source = "flightDto.airport", target = "airport")
     @Mapping(source = "flightDto.reserves", target = "reserves")
     public abstract Flight toEntity(FlightDto flightDto);
 
-    @Mapping(source = "flightDto.airlane",target = "airlane")
+    @Mapping(source = "flightDto.airlane", target = "airlane")
     @Mapping(source = "flightDto.airport", target = "airport")
     @Mapping(source = "flightDto.reserves", target = "reserves")
     public abstract List<Flight> toListEntity(List<FlightDto> flightDtos);
 
     @Named("withoutId")
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "flight.airlane.id",target = "airlane")
+    @Mapping(source = "flight.airlane.id", target = "airlane")
     @Mapping(source = "flight.airport.id", target = "airport")
     @Mapping(source = "flight.reserves", target = "reserves")
     public abstract FlightDto toDto(Flight flight);
 
     @Named("listWithoutId")
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "flight.airlane.id",target = "airlane")
+    @Mapping(source = "flight.airlane.id", target = "airlane")
     @Mapping(source = "flight.airport.id", target = "airport")
     @Mapping(source = "flight.reserves", target = "reserves")
     public abstract List<FlightDto> toListDto(List<Flight> flights);
 
-    public Long mapToClientId(Client client){
+    public Long mapToClientId(Client client) {
         return client.getId();
     }
 
-    public Client mapToClient(Long id){
+    public Client mapToClient(Long id) {
         return clientRepository.findById(id).get();
     }
 
-    public Long mapToAirlaneId(Airlane airlane){
+    public Long mapToAirlaneId(Airlane airlane) {
         return airlane.getId();
     }
 
-    public Airlane mapToAirlane(Long id){
+    public Airlane mapToAirlane(Long id) {
         return airlaneRepository.findById(id).get();
     }
 
-    public Long mapToAirportId(Airport airport){
+    public Long mapToAirportId(Airport airport) {
         return airport.getId();
     }
 
-    public Airport mapToAirport(Long id){
+    public Airport mapToAirport(Long id) {
         return airportRepository.findById(id).get();
     }
 
-    public Long mapToReserveId(Reserve reserve){
+    public Long mapToReserveId(Reserve reserve) {
         return reserve.getId();
     }
-    public Reserve mapToReserve(Long id){
+
+    public Reserve mapToReserve(Long id) {
         return reserveRepository.findById(id).get();
     }
 
-    public List<Reserve> mapToReserveList(List<Long> ids){
-        List<Reserve> reserves = new ArrayList<Reserve>();
-        for(Long id : ids){
+    public List<Reserve> mapToReserveList(List<Long> ids) {
+        List<Reserve> reserves = new ArrayList<>();
+        for (Long id : ids) {
             reserves.add(mapToReserve(id));
         }
         return reserves;
