a329418852d4b8154dfb837ed16fa7ffb733f42c
IdMarchena <idmarchena@gmail.com>
2024-11-24 14:02:38 -0500

    [ADD] reserve/serviceImpl

--- src/main/java/com/example/airport/serviceimplement/ReserveServiceImpl.java
+++ src/main/java/com/example/airport/serviceimplement/ReserveServiceImpl.java
@@ -1,6 +1,17 @@
 package com.example.airport.serviceimplement;
+import com.example.airport.dtos.ClientDto;
+import com.example.airport.dtos.ReserveDto;
+import com.example.airport.entities.Reserve;
+import com.example.airport.mapper.ClientMapper;
+import com.example.airport.mapper.FlightMapper;
+import com.example.airport.mapper.PassengerMapper;
+import com.example.airport.mapper.ReserveMapper;
+import com.example.airport.repositories.ClientRepository;
+import com.example.airport.repositories.ReserveRepository;
+import com.example.airport.services.ReserveService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -9,7 +20,7 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Lazy))
-public class ReserveServiceImpl {
+public class ReserveServiceImpl implements ReserveService {
     private final ClientRepository clientRepository;
     private ReserveMapper reserveMapper;
     private ClientMapper clientMapper;
