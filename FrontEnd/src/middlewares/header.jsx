969f511a20758ad2943abdafcdf32ed0b3845f94
IdMarchena <idmarchena@gmail.com>
2024-12-01 01:22:54 -0500

    [MOD] middlewares

---
+++ FrontEnd/src/middlewares/header.jsx
@@ -0,0 +1,21 @@
+import '../schemas/header.scss';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="header">
+                    <h1>PichiFly</h1>
+                    <ul className="navbar">
+                        <li><a href="/">Reservar</a></li>
+                        <li><a href="/reservations">Ofertas y destinos</a></li>
+                        <li><a href="/reserves">Tu reserva</a></li>
+                    </ul>
+                    <a className="button type-3 login-button" href="/login">Login</a>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
